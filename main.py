d732603bf2052bcc1dbe2709bae76fbd72d41867
Brian Pfeil <brian.pfeil@gmail.com>
2021-02-19 11:01:12 -0500

    added glue_client_example

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 
 
 def s3_client_example():
@@ -21,11 +22,22 @@ def dynamodb_client_example():
     scan_resp = db.scan(TableName=tables_resp['TableNames'][0])
     print(scan_resp['Items'])
 
+def glue_client_example():
+    client = boto3.client('glue')
+    databases_resp = client.get_databases()
+    for database in databases_resp['DatabaseList']:
+        database_name = database['Name']
+        tables_resp = client.get_tables(DatabaseName=database_name)
+        for table in tables_resp['TableList']:
+            table_name = table['Name']
+            table_resp = client.get_table(DatabaseName=database_name, Name=table_name)
+            print(json.dumps(table_resp, indent=2, sort_keys=True, default=str))
 
 def main():
     s3_client_example()
     s3_resource_example
     dynamodb_client_example()
+    glue_client_example()
 
 
 if __name__ == "__main__":
